4a0be401148a3a4a1a6251b40e003fb5faa28f1a
Alex Mendez Jacinto <lrs17110677@purisima.tecnm.mx>
2020-09-29 13:01:24 -0500

    SPAV2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SPACHIDO'`, () => {
+  it(`should have as title 'SPA'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SPACHIDO');
+    expect(app.title).toEqual('SPA');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('SPACHIDO app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('SPA app is running!');
   });
 });
